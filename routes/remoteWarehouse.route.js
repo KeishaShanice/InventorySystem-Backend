3d5b5b8c72b9754f2cda74d5c59b37681b4aa702
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2022-10-04 18:28:12 -0500

    Updated warehouse paths

--- routes/remoteWarehouse.route.js
+++ routes/remoteWarehouse.route.js
@@ -1,11 +1,20 @@
 const router = require('express').Router()
-const { findAllRemoteWarehouses, createRemoteWarehouse } = require('../controllers/remoteWarehouse.controller.js')
+const { findAllProductsRemoteWarehouses,findAllRemoteWarehouses, findRemoteWarehouseById, createRemoteWarehouse, updateRemoteWarehouse, deleteRemoteWarehouseById } = require('../controllers/remoteWarehouse.controller.js')
+
+// Validate ObjectId middleware
+const validateObjectId = (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        res.status(204).send()
+    } else {
+        next()
+    }
+}
 
 
-// Find All Remote Warehouses
-router.get('/', async (req, res) => {
+// Find All Products at Remote Warehouses
+router.get('/remoteproducts', async (req, res) => {
     try {
-        const remoteWarehouses = await findAllRemoteWarehouses()
+        const remoteWarehouses = await findAllProductsRemoteWarehouses()
         res.status(200).json(remoteWarehouses)
     } catch (err) {
         res.status(500).json(err)
@@ -13,11 +22,52 @@ router.get('/', async (req, res) => {
 })
 
 
+// Get all remote warehouses
+router.get('/remote', async (req, res) => {
+    const remoteWarehouse = await findAllRemoteWarehouses()
+    res.json(remoteWarehouse)
+})
+
+
+// Get remote warehouse by ID
+router.get('/:id', validateObjectId, async (req, res) => {
+    try {
+        const remoteWarehouse = await findRemoteWarehouseById(req.params.id)
+        res.json(remoteWarehouse)
+    } catch (err) {
+        console.log(err)
+        res.status(err?.status ?? 500).json(err)
+    }
+})
+
+
 // Create a Remote Warehouse
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const remoteWarehouse = await createRemoteWarehouse(req.body)
         res.statusMessage(201).json(remoteWarehouse)
+    } catch (err) {
+        res.status(err?.status ?? 500).json(err)
+    }
+})
+
+
+//update a warehouse
+router.put('/:id', validateObjectId, async (req, res) => {
+    try {
+        await updateRemoteWarehouse(req.params.id, req.body)
+        res.send()
+    } catch (err) {
+        res.status(err?.status ?? 500).json(err)
+    }
+})
+
+
+//delete a warehouse
+router.delete('/:id', validateObjectId, async (req, res) => {
+    try {
+        await deleteRemoteWarehouseById(req.params.id)
+        res.send()
     } catch (err) {
         res.status(err?.status ?? 500).json(err)
     }
